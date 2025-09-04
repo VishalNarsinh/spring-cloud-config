d4d8bc966aede899b37b908fdc8e32c6894ef1f3
Vishal Narsinh <vishal@tryspeed.com>
2025-09-04 16:23:53 +0530

    Initial commit

---
+++ cards/src/main/java/com/vishal/cards/CardsApplication.java
@@ -0,0 +1,18 @@
+package com.vishal.cards;
+
+import com.vishal.cards.dto.CardsContactInfoDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(value = {
+        CardsContactInfoDto.class
+})
+public class CardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardsApplication.class, args);
+    }
+
+}
---
+++ loans/src/main/java/com/vishal/loans/LoansApplication.java
@@ -0,0 +1,18 @@
+package com.vishal.loans;
+
+import com.vishal.loans.dto.LoansContactInfoDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(value = {
+        LoansContactInfoDto.class
+})
+public class LoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansApplication.class, args);
+    }
+
+}
