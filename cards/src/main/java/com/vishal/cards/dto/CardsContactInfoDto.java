80bec4ceac786208b6c02ee1b40737b55869ee90
VishalNarsinh <vishalnarsinh@gmail.com>
2025-09-24 17:50:45 +0530

    feat: Add Spring Cloud Bus for dynamic config refresh

--- cards/src/main/java/com/vishal/cards/dto/CardsContactInfoDto.java
+++ cards/src/main/java/com/vishal/cards/dto/CardsContactInfoDto.java
@@ -1,10 +1,16 @@
 package com.vishal.cards.dto;
 
+import lombok.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String,String> contactDetails, List<String> onCallSupport) {
+public class CardsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
